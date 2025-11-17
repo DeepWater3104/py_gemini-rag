a665bfd4477a8f0c1f6a0dde4de4bb1de7275789
DeepWater3104 <redcurry1106@gmail.com>
2025-11-17 16:44:03 +0900

    update

--- query_rag.py
+++ query_rag.py
@@ -14,6 +14,7 @@ client = genai.Client(api_key=api_key)
 # --- ストア名を設定 ---
 #FILE_SEARCH_STORE_NAME = "fileSearchStores/neruon-documentation-rag-st-2fcechk5416c"
 FILE_SEARCH_STORE_NAME = "fileSearchStores/neruon-documentation-rag-st-t7508m67c4cc"
+FILE_SEARCH_STORE_NAME = "fileSearchStores/neruon-documentation-rag-st-jplutjmypeph"
 
 # ▼▼▼【ここからが新しい関数】▼▼▼
 def is_question_about_nrn(question: str) -> bool:
